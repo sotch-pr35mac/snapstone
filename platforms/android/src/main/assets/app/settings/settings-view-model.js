41345c075e56706d85fdd0a999a05d6ac5195322
Daniel Michelin <k14dm01@kzoo.edu>
2017-09-29 15:36:41 -0400

    actually removed directory

--- platforms/android/src/main/assets/app/settings/settings-view-model.js
+++
@@ -1,9 +0,0 @@
-var Observable = require('data/observable').Observable;
-
-function SettingsViewModel() {
-  var viewModel = new Observable();
-
-  return viewModel;
-}
-
-module.exports = SettingsViewModel;
--- platforms/android/src/main/assets/app/word-detail/word-detail-view-model.js
+++
@@ -1,9 +0,0 @@
-var Observable = require('data/observable').Observable;
-
-function WordDetailViewModel() {
-  var viewModel = new Observable();
-
-  return viewModel;
-}
-
-module.exports = WordDetailViewModel;
