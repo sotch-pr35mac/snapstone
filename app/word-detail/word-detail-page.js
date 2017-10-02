57c0b1f9ac683aac14c67c1e8246f957f8e7715b
Preston Stosur-Bassett <preston@stosur.info>
2017-10-01 20:32:36 -0400

    feat(ocr): added ocr support
    playing around with what we can do with ocr

--- app/word-detail/word-detail-page.js
+++ app/word-detail/word-detail-page.js
@@ -2,7 +2,7 @@
  *  @file         ::  app/word-detail/word-detail-page.js
  *  @authors      ::  Preston Wang-Stosur-Bassett <preston.wang-stosur-bassett14@kzoo.edu> (IF YOU EDIT THIS FILE, ADD YOUR NAME AND A METHOD OF CONTACT TO THIS LINE)
  *  @created      ::  Sept 24, 2017
- *  @updated      ::  Sept 29, 2017 - Preston Wang-Stosur-Bassett (IF YOU EDIT THIS FILE, REPLACE THIS LINE WITH YOUR NAME AND THE DATE, AND ADD YOURSELF TO THE LIST OF AUTHORS)
+ *  @updated      ::  Oct 1, 2017 - Preston Wang-Stosur-Bassett - Added OCR dependencies as a test / proof of concept (IF YOU EDIT THIS FILE, REPLACE THIS LINE WITH YOUR NAME AND THE DATE, AND ADD YOURSELF TO THE LIST OF AUTHORS)
  *  @description  ::  This file contains the main logic for the word detail page, which is where the app navigates after a picture is taken
 */
 
@@ -10,7 +10,10 @@
 var gestures = require('ui/gestures'); // Gestures Module to handle gestures and swipes
 var frameModule = require('ui/frame'); // Frame Module that handles views and navigation
 var WordDetailViewModel = require('./word-detail-view-model'); // The Model of the word detail page
+var OCRPlugin = require('nativescript-ocr'); // Require the nativescript-orc pluggin, which makes it easy for us to use Tesseract.js inside of our nativescript app
+var imageSource = require('image-source');
 
+var ocr = new OCRPlugin.OCR();
 var wordDetailViewModel = new WordDetailViewModel();
 
 // Gloabl navigateHome object that defines the behavior for navigating home
@@ -36,6 +39,30 @@ function onNavigatingTo(args) {
 
   // Set the image view
   imageView.src = imageData.param1.src;
+
+  var readableImage = imageSource.fromFile(imageData.param1.src.android);
+  //var readableImage = imageSource.fromFile('~/test-ocr-nativescript.png');
+
+  console.log(JSON.stringify(readableImage));
+
+  /*
+   *  TODO: Get this part working fully
+  */
+  // Read the text in the image, and display it to the console
+  ocr.retrieveText({
+    image: readableImage,
+    language: 'chi_sim'
+  }).then(
+    function(result) {
+      alert(result.text);
+      console.log('Result: ' + result.text);
+    },
+    function(error) {
+      alert('there was some sort of error... idk.. read the logs.');
+      alert(error);
+      console.log('Error: ' + error);
+    }
+  );
 
   // Define swipable gestures and their actions
   var myStack = page.getViewById('swipable');
