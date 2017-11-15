21a1c3dc892be5b2efcfecf98682962ef6830cee
Preston Stosur-Bassett <preston@stosur.info>
2017-11-14 22:57:02 -0500

    got remove from bookmarks functional

--- app/bookmark-detail/bookmark-detail.js
+++ app/bookmark-detail/bookmark-detail.js
@@ -9,6 +9,8 @@
 var gestures = require('ui/gestures');
 var frameModule = require('ui/frame');
 var BookmarkDetailViewModel = require('./bookmark-detail-view-model');
+var http = require('http');
+var appSettings = require('application-settings');
 
 var bookmarkDetailViewModel = new BookmarkDetailViewModel();
 
@@ -23,6 +25,9 @@ var navigateHome = {
 	}
 };
 
+var wordObject;
+var serverURL = "https://safe-temple-72583.herokuapp.com";
+
 // This function is loaded when navigating to the page
 function onNavigatingTo(args)
 {
@@ -32,6 +37,8 @@ function onNavigatingTo(args)
   var data = page.navigationContext;
 
   page.bindingContext = data;
+
+  wordObject = data;
 
   page.getViewById('simplified').text = "Simplified: " + data.simplified;
   page.getViewById('traditional').text = "Traditional: " + data.traditional;
@@ -46,3 +53,41 @@ exports.goBack = function()
 {
 	frameModule.topmost().navigate(navigateHome);
 };
+
+exports.removeBookmark = function() {
+  http.request({
+    url: serverURL + "/user/login",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    // Please forgive my use of plaintext username and password
+    content: JSON.stringify({ username: appSettings.getString("username"), password: appSettings.getString("password")})
+  }).then(function(response) {
+    response = response.content.toJSON();
+    if(response.error) {
+
+      console.log("There was an error logging in the user and getting the user data.");
+      console.log(response.status);
+      console.log(response.message);
+      console.log(response.error);
+
+    } else {
+      http.request({
+        url: serverURL + '/bookmarks/remove',
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        content: JSON.stringify(wordObject)
+      }).then(function(response) {
+        response = response.content.toJSON();
+        if(response.error) {
+          console.log("There was an error adding a bookmark.");
+          console.log(response.status);
+          console.log(response.message);
+          console.log(response.error);
+        } else {
+          alert("Word removed from bookmarks!");
+          frameModule.topmost().navigate(navigateHome);
+        }
+      })
+    }
+  });
+}
